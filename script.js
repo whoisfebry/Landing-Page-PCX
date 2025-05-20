c0ee13de32d18e5bf9f9d9cd23d18d4593ef5b53
whoisfebry <febryibme19@gmail,com>
2025-05-20 14:12:47 +0700

    Update landing page

--- script.js
+++ script.js
@@ -44,14 +44,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
-// window.addEventListener('load', function () {
-//     const loader = document.getElementById('loader');
-//     const main = document.getElementById('main-content');
-//     loader.style.opacity = '0';
-//     setTimeout(() => {
-//         loader.style.display = 'none';
-//         main.style.display = 'block';
-//     }, 600000); // waktu fade out
-// });
-
